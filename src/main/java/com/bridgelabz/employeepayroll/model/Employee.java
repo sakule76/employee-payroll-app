18cc46a77c788ac23a4ba3eb7d5f0bda985d2556
Manan Gupta <manangupta27.mg@gmail.com>
2025-04-17 13:21:11 +0530

    [Manan] ADD:Added employee model which is entity class maps to database table

---
+++ src/main/java/com/bridgelabz/employeepayroll/model/Employee.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.employeepayroll.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employeeId;
+    private String name;
+    private double salary;
+
+    public Employee(){}
+
+    public Employee(long employeeId, String name, double salary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
